dce671e3ac3042ffaa5254ee7779304e4c44584a
muhammadfariddd <muhammadfariddd2004@gmail.com>
2025-01-12 04:21:58 +0700

    Fix merge conflicts and update configurations

--- src/scripts/views/template-creator.js
+++ src/scripts/views/template-creator.js
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 import CONFIG from '../globals/config.js';
-=======
-import CONFIG from '../globals/config';
->>>>>>> 830f8be6b870cd26c773335c3572f04b403cf706
 
 const createDetailTemplate = (restaurant) => `
   <div class="restaurant-detail">
     <h2 class="restaurant-item__title">${restaurant.name}</h2>
-<<<<<<< HEAD
     <img class="restaurant-item__image" 
       src="${CONFIG.MEDIUM_BASE_IMAGE_URL}${restaurant.pictureId}" 
       alt="${restaurant.name}"
       onerror="this.onerror=null; this.src='./images/placeholder.jpg';" />
-=======
-    <img class="restaurant-item__image" src="${CONFIG.MEDIUM_BASE_IMAGE_URL}${
-  restaurant.pictureId
-}" alt="${restaurant.name}" />
->>>>>>> 830f8be6b870cd26c773335c3572f04b403cf706
     
     <div class="makan__info">
       <h3>Detail Restaurant</h3>
@@ -30,25 +20,15 @@ const createDetailTemplate = (restaurant) => `
       <h4>Menu Makanan</h4>
       <ul>
         ${restaurant.menus.foods
-<<<<<<< HEAD
           .map((food) => `<li>${food.name}</li>`)
           .join('')}
-=======
-    .map((food) => `<li>${food.name}</li>`)
-    .join('')}
->>>>>>> 830f8be6b870cd26c773335c3572f04b403cf706
       </ul>
       
       <h4>Menu Minuman</h4>
       <ul>
         ${restaurant.menus.drinks
-<<<<<<< HEAD
           .map((drink) => `<li>${drink.name}</li>`)
           .join('')}
-=======
-    .map((drink) => `<li>${drink.name}</li>`)
-    .join('')}
->>>>>>> 830f8be6b870cd26c773335c3572f04b403cf706
       </ul>
       
       <h4>Rating</h4>
@@ -58,7 +38,6 @@ const createDetailTemplate = (restaurant) => `
         <h4>Customer Reviews</h4>
         <div class="review-list">
           ${restaurant.customerReviews
-<<<<<<< HEAD
             .map(
               (review) => `
                 <div class="review-item">
@@ -70,19 +49,6 @@ const createDetailTemplate = (restaurant) => `
             )
             .join('')}
         </div>
-=======
-    .map(
-      (review) => `
-            <div class="review-item">
-              <p class="review-name">${review.name}</p>
-              <p class="review-date">${review.date}</p>
-              <p class="review-text">${review.review}</p>
-            </div>
-          `
-    )
-    .join('')}
-         </div>
->>>>>>> 830f8be6b870cd26c773335c3572f04b403cf706
         <div id="reviewFormContainer"></div>
       </div>
     </div>
@@ -105,7 +71,6 @@ const createLikedButtonTemplate = () => `
 
 const createItemTemplate = (restaurant) => `
   <div class="restaurant-item">
-<<<<<<< HEAD
     <img class="restaurant-item__image lazyload" 
       data-src="${CONFIG.SMALL_BASE_IMAGE_URL}${restaurant.pictureId}" 
       alt="${restaurant.name}"
@@ -115,11 +80,6 @@ const createItemTemplate = (restaurant) => `
       <h2 class="restaurant-item__title">
         <a href="/#/detail/${restaurant.id}">${restaurant.name}</a>
       </h2>
-=======
-    <img class="restaurant-item__image" src="${CONFIG.SMALL_BASE_IMAGE_URL}${restaurant.pictureId}" alt="${restaurant.name}">
-    <div class="restaurant-item__content">
-      <h2 class="restaurant-item__title"><a href="/#/detail/${restaurant.id}">${restaurant.name}</a></h2>
->>>>>>> 830f8be6b870cd26c773335c3572f04b403cf706
       <p class="restaurant-item__desc">${restaurant.city} | Rating: ${restaurant.rating}⭐</p>
       <p class="restaurant-item__description">${restaurant.description}</p>
     </div>
