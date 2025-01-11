e294c10d1f14bbaebdb8787690d0c451df240ae9
muhammadfariddd <muhammadfariddd2004@gmail.com>
2025-01-12 04:34:15 +0700

    Fix merge conflicts in config files and webpack

--- webpack.common.js
+++ webpack.common.js
@@ -80,6 +80,7 @@ export default {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
         },
+<<<<<<< Updated upstream
         {
           from: path.resolve(__dirname, 'src/public/images/'),
           to: path.resolve(__dirname, 'dist/images/'),
@@ -88,6 +89,8 @@ export default {
           from: path.resolve(__dirname, 'src/public/fonts/fontawesome'),
           to: path.resolve(__dirname, 'dist/fonts/fontawesome'),
         },
+=======
+>>>>>>> Stashed changes
       ],
     }),
     new BundleAnalyzerPlugin({
