28b2052acec46559dfdc9d9879f2a16882a3d813
muhammadfariddd <muhammadfariddd2004@gmail.com>
2025-01-13 05:14:52 +0700

    Fix: Remove all merge conflicts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,7 @@ import pluginJs from '@eslint/js';
 import daStyle from 'eslint-config-dicodingacademy';
 
 export default [
-<<<<<<< HEAD
-  { languageOptions: { globals: globals.browser } },
-=======
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
->>>>>>> 830f8be6b870cd26c773335c3572f04b403cf706
   pluginJs.configs.recommended,
   daStyle,
 ];
--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,17 +1,6 @@
 import { Workbox } from 'workbox-window';
 
 const swRegister = async () => {
-<<<<<<< HEAD
-  if ('serviceWorker' in navigator) {
-    const workbox = new Workbox('./sw.bundle.js');
-
-    try {
-      await workbox.register();
-      console.log('Service worker registered');
-    } catch (error) {
-      console.log('Failed to register service worker', error);
-    }
-=======
   if (!('serviceWorker' in navigator)) {
     console.log('Service Worker not supported in the browser');
     return;
@@ -24,7 +13,6 @@ const swRegister = async () => {
     console.log('Service worker registered');
   } catch (error) {
     console.log('Failed to register service worker', error);
->>>>>>> 830f8be6b870cd26c773335c3572f04b403cf706
   }
 };
 
